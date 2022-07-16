270b27d90591a540c254e49aac7c5b311043f991
cupcakearmy <hi@nicco.io>
2022-07-16 17:29:22 +0200

    update deps and add esm support

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 type Intervals =
-	'millisecond'
-	| 'milliseconds'
-	| 'ms'
-	| 'second'
-	| 'seconds'
-	| 's'
-	| 'minute'
-	| 'minutes'
-	| 'm'
-	| 'hour'
-	| 'hours'
-	| 'h'
-	| 'day'
-	| 'days'
-	| 'd'
-	| 'week'
-	| 'weeks'
-	| 'w'
-	| 'year'
-	| 'years'
-	| 'y'
+  | 'millisecond'
+  | 'milliseconds'
+  | 'ms'
+  | 'second'
+  | 'seconds'
+  | 's'
+  | 'minute'
+  | 'minutes'
+  | 'm'
+  | 'hour'
+  | 'hours'
+  | 'h'
+  | 'day'
+  | 'days'
+  | 'd'
+  | 'week'
+  | 'weeks'
+  | 'w'
+  | 'year'
+  | 'years'
+  | 'y'
 
 const Millisecond = 1
 const Second = Millisecond * 1000
@@ -34,132 +34,128 @@ export type HumanizerReturnFN = (duration: Duration) => string
 export type Humanizer = [HumanizerTestFN, HumanizerReturnFN][]
 
 const defaultHumanizer: Humanizer = [
-	[d => d.years() > 0, d => `${d.years()} years`],
-	[d => d.weeks() > 1, d => `${d.weeks()} weeks`],
-	[d => d.days() > 0, d => `${d.days()} days`],
-	[d => d.hours() > 0, d => `${d.hours()} hours`],
-	[d => d.minutes() > 5, d => `${d.minutes()} minutes`],
-	[d => d.minutes() > 0, _ => `a few minutes`],
-	[() => true, () => `a moment`],
+  [(d) => d.years() > 0, (d) => `${d.years()} years`],
+  [(d) => d.weeks() > 1, (d) => `${d.weeks()} weeks`],
+  [(d) => d.days() > 0, (d) => `${d.days()} days`],
+  [(d) => d.hours() > 0, (d) => `${d.hours()} hours`],
+  [(d) => d.minutes() > 5, (d) => `${d.minutes()} minutes`],
+  [(d) => d.minutes() > 0, (_) => `a few minutes`],
+  [() => true, () => `a moment`],
 ]
 
-
 export class Duration {
+  private duration: number
 
-	private duration: number
+  constructor(amount: number, interval: Intervals) {
+    this.duration = Duration.ProcessInterval(amount, interval)
+  }
 
-	constructor(amount: number, interval: Intervals) {
-		this.duration = Duration.ProcessInterval(amount, interval)
-	}
+  private static ProcessInterval(amount: number, interval: Intervals): number {
+    switch (interval.toLowerCase()) {
+      case 'millisecond':
+      case 'milliseconds':
+      case 'ms':
+        return amount * Millisecond
+      case 'second':
+      case 'seconds':
+      case 's':
+        return amount * Second
+      case 'minute':
+      case 'minutes':
+      case 'm':
+        return amount * Minute
+      case 'hour':
+      case 'hours':
+      case 'h':
+        return amount * Hour
+      case 'day':
+      case 'days':
+      case 'd':
+        return amount * Day
+      case 'week':
+      case 'weeks':
+      case 'w':
+        return amount * Week
+      case 'year':
+      case 'years':
+      case 'y':
+        return amount * Year
+      default:
+        throw new Error('Wrong interval')
+    }
+  }
 
-	private static ProcessInterval(amount: number, interval: Intervals): number {
-		switch (interval.toLowerCase()) {
-			case 'millisecond':
-			case 'milliseconds':
-			case 'ms':
-				return amount * Millisecond
-			case 'second':
-			case 'seconds':
-			case 's':
-				return amount * Second
-			case 'minute':
-			case 'minutes':
-			case 'm':
-				return amount * Minute
-			case 'hour':
-			case 'hours':
-			case 'h':
-				return amount * Hour
-			case 'day':
-			case 'days':
-			case 'd':
-				return amount * Day
-			case 'week':
-			case 'weeks':
-			case 'w':
-				return amount * Week
-			case 'year':
-			case 'years':
-			case 'y':
-				return amount * Year
-			default:
-				throw new Error('Wrong interval')
-		}
-	}
+  public add(amount: number, interval: Intervals): Duration {
+    this.duration += Duration.ProcessInterval(amount, interval)
+    return this
+  }
 
-	public add(amount: number, interval: Intervals): Duration {
-		this.duration += Duration.ProcessInterval(amount, interval)
-		return this
-	}
+  public subtract(amount: number, interval: Intervals): Duration {
+    this.duration -= Duration.ProcessInterval(amount, interval)
+    return this
+  }
 
-	public subtract(amount: number, interval: Intervals): Duration {
-		this.duration -= Duration.ProcessInterval(amount, interval)
-		return this
-	}
+  public asMilliseconds(): number {
+    return this.duration
+  }
 
-	public asMilliseconds(): number {
-		return this.duration
-	}
+  public asSeconds(): number {
+    return this.duration / Second
+  }
 
-	public asSeconds(): number {
-		return this.duration / Second
-	}
+  public asMinutes(): number {
+    return this.duration / Minute
+  }
 
-	public asMinutes(): number {
-		return this.duration / Minute
-	}
+  public asHours(): number {
+    return this.duration / Hour
+  }
 
-	public asHours(): number {
-		return this.duration / Hour
-	}
+  public asDays(): number {
+    return this.duration / Day
+  }
 
-	public asDays(): number {
-		return this.duration / Day
-	}
+  public asWeeks(): number {
+    return this.duration / Week
+  }
 
-	public asWeeks(): number {
-		return this.duration / Week
-	}
+  public asYears(): number {
+    return this.duration / Year
+  }
 
-	public asYears(): number {
-		return this.duration / Year
-	}
+  public milliseconds(): number {
+    return ((((((this.duration % Year) % Day) % Hour) % Minute) % Second) / Millisecond) | 0
+  }
 
-	public milliseconds(): number {
-		return (this.duration % Year % Day % Hour % Minute % Second) / Millisecond | 0
-	}
+  public seconds(): number {
+    return (((((this.duration % Year) % Day) % Hour) % Minute) / Second) | 0
+  }
 
-	public seconds(): number {
-		return (this.duration % Year % Day % Hour % Minute) / Second | 0
-	}
+  public minutes(): number {
+    return ((((this.duration % Year) % Day) % Hour) / Minute) | 0
+  }
 
-	public minutes(): number {
-		return (this.duration % Year % Day % Hour) / Minute | 0
-	}
+  public hours(): number {
+    return (((this.duration % Year) % Day) / Hour) | 0
+  }
 
-	public hours(): number {
-		return (this.duration % Year % Day) / Hour | 0
-	}
+  public days(): number {
+    return ((this.duration % Year) / Day) | 0
+  }
 
-	public days(): number {
-		return (this.duration % Year) / Day | 0
-	}
+  public weeks(): number {
+    return (this.duration / Week) | 0
+  }
 
-	public weeks(): number {
-		return (this.duration) / Week | 0
-	}
+  public years(): number {
+    return (this.duration / Year) | 0
+  }
 
-	public years(): number {
-		return (this.duration) / Year | 0
-	}
+  public humanize(humanizer?: Humanizer): string {
+    if (!humanizer) humanizer = defaultHumanizer
 
-	public humanize(humanizer?: Humanizer): string {
-		if (!humanizer) humanizer = defaultHumanizer
+    for (const [control, value] of humanizer) if (control(this)) return value(this)
 
-		for (const [control, value] of humanizer)
-			if (control(this))
-				return value(this)
-
-		return ''
-	}
+    return ''
+  }
 }
